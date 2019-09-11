728d1a729b3f9a810d01ac7c440f308dae5a8c3b
Dillon Coffman <tug40394@temple.edu>
2019-09-11 09:29:23 -0400

    Added and centered button horizontally; Added OnClickListener so that when the button is clicked, it changes the textView's text

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -2,12 +2,27 @@ package edu.temple.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewToChange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewToChange = findViewById(R.id.textViewToChange);
+
+        findViewById(R.id.clickButton)
+                .setOnClickListener(new View.OnClickListener() {
+                    // Change text with id of textViewToChange on click
+                    @Override
+                    public void onClick(View v) {
+                        textViewToChange.setText("You have clicked the button");
+                    }
+                });
     }
 }
